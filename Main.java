36a581920af5d925eb875160ab725f8e7b0f6823
Vadym Shakhraichuk <64581387+vvs8@users.noreply.github.com>
2021-02-10 14:23:33 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,23 @@
 class Main {
   public static void main(String[] args) {
-    
-		StackQ Q = new StackQ(); 
-		
-		Q.enQueue("A"); 
-		Q.enQueue("B"); 
-		Q.enQueue("C"); 
+   	StackQ Q = new StackQ(); 
+	Q.enQueue("A"); 
+	Q.enQueue("B"); 
+	Q.enQueue("C"); 
 
-		System.out.println(Q.deQueue()); 
-		System.out.println(Q.deQueue()); 
-		System.out.println(Q.deQueue()); 
-    System.out.println(Q.deQueue()); //null
+	System.out.println(Q.deQueue()); 
+	System.out.println(Q.deQueue()); 
+	System.out.println(Q.deQueue()); 
+	System.out.println(Q.deQueue()); //null
 
-    Q.enQueue("A"); 
-    Q.enQueue("B"); 
-    Q.enQueue("C"); 
-    System.out.println(Q.deQueue()); //A
-    System.out.println(Q.deQueue());  //B
-    Q.enQueue("D");
-    System.out.println(Q.deQueue()); //C
-		System.out.println(Q.deQueue()); //D
-    System.out.println(Q.deQueue()); //null
-		
-	 
+        Q.enQueue("A"); 
+        Q.enQueue("B"); 
+        Q.enQueue("C"); 
+        System.out.println(Q.deQueue()); //A
+        System.out.println(Q.deQueue());  //B
+        Q.enQueue("D");
+        System.out.println(Q.deQueue()); //C
+	System.out.println(Q.deQueue()); //D
+        System.out.println(Q.deQueue()); //null 
   }
 }
