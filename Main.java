cfb676bb1beffed4680e1ef6dce8b1f506eab4cc
Vadym Shakhraichuk <64581387+vvs8@users.noreply.github.com>
2021-02-10 14:21:43 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    
+		StackQ Q = new StackQ(); 
+		
+		Q.enQueue("A"); 
+		Q.enQueue("B"); 
+		Q.enQueue("C"); 
+
+		System.out.println(Q.deQueue()); 
+		System.out.println(Q.deQueue()); 
+		System.out.println(Q.deQueue()); 
+    System.out.println(Q.deQueue()); //null
+
+    Q.enQueue("A"); 
+    Q.enQueue("B"); 
+    Q.enQueue("C"); 
+    System.out.println(Q.deQueue()); //A
+    System.out.println(Q.deQueue());  //B
+    Q.enQueue("D");
+    System.out.println(Q.deQueue()); //C
+		System.out.println(Q.deQueue()); //D
+    System.out.println(Q.deQueue()); //null
+		
+	 
+  }
+}
